eb25cbc9ea3a88a60d70451ceff8e5089cee4fe6
Wayllon Castro Santos <wayllon.csantos@gmail.com>
2021-03-19 21:25:26 -0300

    Adicionando recurso Rest aos Hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { useGet } from './hooks/useGet'
+import Rest from './hooks/rest'
 
-const URL = 'https://my-expenses-react-app-default-rtdb.firebaseio.com/transaction/2021-03.json'
+const baseURL = 'https://my-expenses-react-app-default-rtdb.firebaseio.com/'
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 const App = () => {
-  const data = useGet(URL)
+  const data = useGet('transaction/2021-03/expenses')
+  const [postData, post] = usePost('transaction/2021-03/expenses')
+  const [deleteData, remove] = useDelete()
+  
+  const newSave = () => {
+    post({ description: 'Luz', value: '130.27'})
+  }
+
+  const doRemove = () => {
+    remove('transaction/2021-03/expenses/-MWC-xhp3obuewL7KGNa')
+  }
 
   return (
     <div>
-      {JSON.stringify(data)}
+      { JSON.stringify(data) }
+      <button onClick={newSave}>Salvar</button>
+      <pre> { JSON.stringify(postData) } </pre>
+      <button onClick={doRemove}>Excluir</button>
+      <pre> { JSON.stringify(deleteData) } </pre>
     </div>
   )
 }
